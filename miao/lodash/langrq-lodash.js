c15282e4d1db0580d4c2161a16e9262ad2878d4b
unknown <10703554136@qq.com>
2020-12-08 16:16:02 +0800

    chunk

--- miao/lodash/langrq-lodash.js
+++ miao/lodash/langrq-lodash.js
@@ -8,7 +8,22 @@ var langrq = function () {
     }
     return result
   }
+  function chunk(ary, n) {
+    var result = []
+    for (var i = 0; i < ary.length; i) {
+      var digit = []
+      for (var j = 0; j < n; j++) {
+        digit.push(ary[i])
+        i++
+        if (!ary[i]) break
+      }
+      result.push(digit)
+
+    }
+    return result
+  }
   return {
+    chunk,
     compact,
   }
 
