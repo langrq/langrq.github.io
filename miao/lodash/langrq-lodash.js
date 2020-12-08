56c39a6c580a82280f17ce77859eae00ebe58598
unknown <10703554136@qq.com>
2020-12-08 17:16:15 +0800

    drop

--- miao/lodash/langrq-lodash.js
+++ miao/lodash/langrq-lodash.js
@@ -8,6 +8,8 @@ var langrq = function () {
     }
     return result
   }
+
+
   function chunk(ary, n) {
     var result = []
     for (var i = 0; i < ary.length; i) {
@@ -22,9 +24,63 @@ var langrq = function () {
     }
     return result
   }
+
+  function compact(ary) {
+    var result = []
+    for (var i = 0; i < ary.length; i++) {
+      if (typeof ary[i] === "number" && ary[i] != 0) {
+        result.push(ary[i])
+      }
+    }
+    return result
+  }
+
+
+  function join(ary, n) {
+    var result = ''
+    for (var i = 0; i < ary.length - 1; i++) {
+      result += ary[i] + n
+    }
+    result = result + ary[i]
+    return result
+  }
+
+  function last(ary) {
+    return result = ary[ary.length - 1]
+  }
+
+  function lastIndexOf(ary, value, start) {
+    var n = ary.length
+    if (start == undefined) start = n - 1
+    for (var i = start; i >= 0; i--) {
+      if (ary[i] === value) {
+        return i
+      }
+    }
+    return -1
+  }
+
+  function drop(ary, num) {
+    var n = ary.length
+    var result = []
+    if (num == undefined) num = 1
+    var result = []
+    for (var i = num; i < n; i++) {
+      result.push(ary[i])
+    }
+    return result
+  }
+
+
+
+
   return {
     chunk,
     compact,
+    compact,
+    join,
+    last,
+    lastIndexOf,
   }
 
 }()
