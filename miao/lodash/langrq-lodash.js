2727100273628ab3a709048188719d5c159c0a6e
unknown <10703554136@qq.com>
2020-12-09 09:03:19 +0800

    finddeep

--- miao/lodash/langrq-lodash.js
+++ miao/lodash/langrq-lodash.js
@@ -62,9 +62,65 @@ var langrq = function () {
     return result
   }
 
+  function dropRight(ary, num) {
+    if (num == undefined) num = 1
+    if (num > ary.length) num = ary.length
+    ary.length = ary.length - num
+    return ary
+  }
+
+  function fill(ary, value, start = 0, end = ary.length) {
+    for (var i = start; i < end; i++) {
+      ary[i] = value
+    }
+    return ary
+  }
 
 
+  function findIndex(ary, predicate, start = 0) {
 
+    for (var i = start; i < ary.length; i++) {
+      if (typeof predicate == 'function') {
+
+      } else if (typeof predicate == 'object') {
+
+      } else if (typeof predicate == 'string') {
+
+      }
+
+    }
+    return -1
+  }
+
+  function findLastindex() {
+
+  }
+
+  function flatten(ary) {
+    var result = []
+    for (var i = 0; i < ary.length; i++) {
+      if (typeof ary[i] == 'string') {
+        result.push(ary[i])
+      } else if (typeof ary[i] == 'object') {
+        for (var j = 0; j < ary[i].length; j++) {
+          result.push(ary[i][j])
+        }
+      }
+    }
+    return result
+  }
+
+  function flattenDeep(ary, ans = []) {
+    for (var i = 0; i < ary.length; i++) {
+      var result = ary[i]
+      if (typeof result == 'object') {
+        flattenDeep(result, ans)   //递归内部数组
+      } else {
+        ans.push(result)
+      }
+    }
+    return ans
+  }
 
   return {
     chunk,
@@ -74,6 +130,12 @@ var langrq = function () {
     last,
     lastIndexOf,
     drop,
+    dropRight,
+    fill,
+    findIndex,
+    findLastindex,
+    flatten,
+    flattenDeep,
   }
 
 }()
